49d7217267d6f1b98f604cdfd1fc2258d875a330
qianlicao51 <qianlicaodemo@163.com>
2018-07-25 23:03:24 +0800

    初始化

---
+++ src/main/java/com/zhuziym/ap01/Demo1.java
@@ -0,0 +1,13 @@
+package com.zhuziym.ap01;
+
+/**
+ * @author 作者 grq
+ * @version 创建时间：2018年7月25日 下午10:53:26
+ *
+ */
+public class Demo1 {
+	public static void main(String[] args) {
+
+		System.out.println("Demo1.main()");
+	}
+}
